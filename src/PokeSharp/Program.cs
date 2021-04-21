0c42f77c2064c83104963a66cd753ca109a2b14d
Fabiano Conrado <fabianoconrado@gmail.com>
2021-04-20 22:18:14 -0300

    Consultando a API com retorno string

--- src/PokeSharp/Program.cs
+++ src/PokeSharp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace PokeSharp
 {
     class Program
     {
+        readonly static string endpoint = "https://pokeapi.co/api/v2/";
+        readonly static HttpClient clientHttp = new HttpClient();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            clientHttp.DefaultRequestHeaders.Clear();
+            clientHttp.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var stringTask = clientHttp.GetStringAsync($"{endpoint}pokemon/ditto").Result;
+
+
         }
     }
 }
